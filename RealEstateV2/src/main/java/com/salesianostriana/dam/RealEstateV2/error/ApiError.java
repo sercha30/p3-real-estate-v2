ed58b4555dcaed6930dd78054123bcc010091265
Sergio Chamorro Garcia <chamorrogarciasergio@gmail.com>
2021-12-03 14:25:51 +0100

    Implementación de manejo de excepciones personalizadas sin finalizar.

---
+++ RealEstateV2/src/main/java/com/salesianostriana/dam/RealEstateV2/error/ApiError.java
@@ -0,0 +1,23 @@
+package com.salesianostriana.dam.RealEstateV2.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+
+    @NonNull
+    private HttpStatus estado;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+    pattern = "dd/MM/yyyy hh:mm:ss")
+    private LocalDateTime fecha = LocalDateTime.now();
+
+    @NonNull
+    private String mensaje;
+}
